a0f0e00e61a428fb4868f1bdaa626262c767877e
camille-s <camille.seaberry@gmail.com>
2021-01-20 15:01:42 -0500

    Add inner/outer ring regions

--- fetch_scripts/01_fetch_basic_all_geos.R
+++ fetch_scripts/01_fetch_basic_all_geos.R
@@ -5,10 +5,11 @@ library(cwi)
 year <- 2019
 ############################################
 
-regions_short <- cwi::regions[c("Greater New Haven", "Greater Waterbury", "Greater Bridgeport", "Lower Naugatuck Valley", "Greater Hartford")]
+# regions_short <- cwi::regions[c("Greater New Haven", "Greater Waterbury", "Greater Bridgeport", "Lower Naugatuck Valley", "Greater Hartford")]
+regions2 <- cwi::regions[stringr::str_subset(names(cwi::regions), "^\\D.+(County|cities)$", negate = TRUE)]
 
 fetch <- purrr::map(basic_table_nums, 
-                    ~multi_geo_acs(table = ., year = year, towns = "all", regions = regions_short, tracts = "all"))
+                    ~multi_geo_acs(table = ., year = year, towns = "all", regions = regions2, tracts = "all"))
 
 saveRDS(fetch, stringr::str_glue("./fetch_data/acs_basic_{year}_fetch_towns.rds"))
 
